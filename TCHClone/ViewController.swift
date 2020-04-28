a5732fd9d13f467ce72b09bf011290e8029ab1b9
yukifox <yukifox@users.noreply.github.com>
2020-04-28 10:07:12 +0700

    Initial Commit

---
+++ TCHClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TCHClone
+//
+//  Created by Trần Huy on 4/28/20.
+//  Copyright © 2020 Trần Huy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
